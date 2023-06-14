703b782d7da5e7077127a8ace6360ac65f3ff362
liasul <136583892+liasul@users.noreply.github.com>
2023-06-14 15:58:54 +0300

    Create split.py

---
+++ split.py
@@ -0,0 +1,14 @@
+a = list(map(str, input().split()))
+print('\n'.join(a))
+
+a = list(map(str, input().split()))
+print(a[-1], a[1][0]+'.', a[2][0]+'.')
+
+
+a = list(map(int, input().split()))
+print('-'.join(a[0))
+
+
+a = list(map(int, input().split()))
+b = a.reverse()
+print(b)
